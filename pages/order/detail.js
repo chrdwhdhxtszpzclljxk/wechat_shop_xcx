756e1f6424f0840b890d587d31896e86eef19876
毛毛 <20774746@qq.com>
2017-10-13 00:15:01 +0800

    分类列表排版重写，订单详情添加付款入口
    ...

--- pages/order/detail.js
+++ pages/order/detail.js
@@ -3,6 +3,7 @@ var app = getApp();
 Page({
   data:{
     orderId:0,
+    totalm:0,
     orderData:{},
     proData:[],
   },
@@ -31,7 +32,8 @@ Page({
           var ord = res.data.ord;
           that.setData({
             orderData: ord,
-            proData:pro
+            proData:pro,
+            totalm: (parseFloat(ord.price) + parseFloat(ord.yunfei))
           });
         }else{
           wx.showToast({
@@ -49,5 +51,83 @@ Page({
       }
     });
   },
+
+  /**
+   * 微信支付订单
+   */
+  // payOrderByWechat: function(event){
+  //   var orderId = event.currentTarget.dataset.orderId;
+  //   this.prePayWechatOrder(orderId);
+  //   var successCallback = function(response){
+  //     console.log(response);
+  //   }
+  //   common.doWechatPay("prepayId", successCallback);
+  // },
+
+  payOrderByWechat: function (e) {
+    var order_id = e.currentTarget.dataset.orderId;
+    var order_sn = e.currentTarget.dataset.ordersn;
+    if (!order_sn) {
+      wx.showToast({
+        title: "订单异常!",
+        duration: 2000,
+      });
+      return false;
+    }
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Wxpay/wxpay',
+      data: {
+        order_id: order_id,
+        order_sn: order_sn,
+        uid: app.d.userId,
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, // 设置请求的 header
+      success: function (res) {
+        if (res.data.status == 1) {
+          var order = res.data.arr;
+          console.log(order);
+          wx.requestPayment({
+            timeStamp: order.timeStamp,
+            nonceStr: order.nonceStr,
+            package: order.package,
+            signType: 'MD5',
+            paySign: order.paySign,
+            success: function (res) {
+              wx.showToast({
+                title: "支付成功!",
+                duration: 2000,
+              });
+              setTimeout(function () {
+                wx.navigateTo({
+                  url: '../user/dingdan?currentTab=1&otype=deliver',
+                });
+              }, 3000);
+            },
+            fail: function (res) {
+              wx.showToast({
+                title: '支付未成功！',
+                duration: 3000
+              })
+            }
+          })
+        } else {
+          wx.showToast({
+            title: res.data.err,
+            duration: 2000
+          });
+        }
+      },
+      fail: function (e) {
+        // fail
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      }
+    })
+  },  
 
 })
